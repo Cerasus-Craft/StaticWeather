7a02b1b70013fe2b5e8b3126ea00bd623f166533
Dams4K <dams4k@users.noreply.github.com>
2025-07-19 14:48:03 +0200

    no need to print, no need for nms, just work

--- src/main/java/fr/cerasus/staticweather/StaticWeather.java
+++ src/main/java/fr/cerasus/staticweather/StaticWeather.java
@@ -7,9 +7,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class StaticWeather extends JavaPlugin {
     @Override
     public void onEnable() {
-        ConsoleCommandSender sender = Bukkit.getConsoleSender();
-        sender.sendMessage("[StaticWeather] Plugin enabled");
-
         getServer().getPluginManager().registerEvents(new WeatherListener(), this);
     }
 }
